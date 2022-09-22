273d5206389c64bcac95cdb13c04c6d19894428a
Rafael Kenji Nagai <rafael.nagai@arctouch.com>
2022-09-22 14:04:51 -0300

    feat: iOS implemented

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,48 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let eventChannel = FlutterEventChannel(name: "my_app/event/battery", binaryMessenger: controller.binaryMessenger)
+        eventChannel.setStreamHandler(BatteryStreamHandler())
+      
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+class BatteryStreamHandler: NSObject, FlutterStreamHandler {
+    
+    var events : FlutterEventSink?
+    
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.events = events
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        batteryLevelDidChange()
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.batteryLevelDidChange),
+            name: UIDevice.batteryLevelDidChangeNotification,
+            object: nil
+        )
+        
+        return nil
+    }                                                                                                                                                                                        
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.events = nil
+        return nil
+    }
+    
+    @objc private func batteryLevelDidChange(){
+        let device = UIDevice.current
+        if device.batteryState == UIDevice.BatteryState.unknown {
+            self.events!(FlutterError(code: "ERROR_CODE",
+                                 message: "Detailed message",
+                                 details: nil))
+        } else {
+            let batteryLevel = Int(device.batteryLevel * 100)
+            self.events!(batteryLevel)
+        }
+    }
+} 
